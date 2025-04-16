6ecf076067c8ab0df881eb282c9590e4f5d36bbd
unseo Jang <unseojang@unseoui-MacBookPro.local>
2025-04-16 17:46:16 +0900

    CartSummary 추가

--- src/advanced/components/Cart.tsx
+++ src/advanced/components/Cart.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import { CartItem } from './CartItem';
+import { CartSummary } from './CartSummary';
 
 export const Cart = () => {
   const [prodList, setProdList] = useState(() => [
@@ -19,13 +20,7 @@ export const Cart = () => {
         <CartItem id={''} name={''} val={0} quantity={0} on={undefined} />
       </div>
 
-      <div id="cart-total" className="text-xl font-bold my-4">
-        총액: 0원
-        <span className="text-green-500 ml-2">(10.0% 할인 적용)</span>
-        <span id="loyalty-points" className="text-blue-500 ml-2">
-          (포인트: 126)
-        </span>
-      </div>
+      <CartSummary />
 
       <div className="flex items-center mt-4">
         <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded">
---
+++ src/advanced/components/CartSummary.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+export const CartSummary = () => {
+  return (
+    <div id="cart-total" className="text-xl font-bold my-4">
+      총액: 0원
+      <span className="text-green-500 ml-2">(10.0% 할인 적용)</span>
+      <span id="loyalty-points" className="text-blue-500 ml-2">
+        (포인트: 126)
+      </span>
+    </div>
+  );
+};
