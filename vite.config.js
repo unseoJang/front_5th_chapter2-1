8fb7bfa04e5223acbdacca2b739081c4eb2dfd54
unseoJang <qqbck123@naver.com>
2025-04-17 22:34:09 +0900

    index 위치 변경

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vitest/config"
 
 export default defineConfig({
+	base: "/front_5th_chapter2-1/", // ✅ GitHub Pages 배포 경로 설정
 	test: {
 		globals: true,
 		environment: "jsdom",
